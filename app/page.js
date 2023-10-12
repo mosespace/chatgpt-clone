0aa7ec9f69fdbb41ecfa736a533b13fd204f8ba4
mosespace <kisakyemoses50@gmail.com>
2023-10-12 10:56:21 +0300

    feast: initial commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Answers from "@/components/Home";
+
+export default function Home() {
+  return (
+    <main className='pb-[12rem] mt-[4rem] gap-6 flex flex-col justify-center items-center w-full h-full lg:flex lg:items-center lg:flex-col lg:justify-center lg:ml-[20%] lg:w-[80%] text-center'>
+      <Answers />
+    </main>
+  );
+}
