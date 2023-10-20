b44a2a71f8cfcd45f4a569409b054cc84904d99b
mosespace <kisakyemoses50@gmail.com>
2023-10-20 15:39:38 +0300

    feat: modified a file

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
 import Answers from "@/components/Home";
+import getAllChats from "@/libs/getAllChats";
 
-export default function Home() {
+export default async function Home() {
+  // const chats = await getAllChats();
+  // // console.log(chats.chat.id);
+  // const chatIdToFilter = chats.id;
+
+  // const filteredConversations = chats
+  //   .filter((chat) => chat.id === chatIdToFilter)
+  //   .map((chat) => chat.conversation);
+
+  // console.log(filteredConversations);
   return (
     <main className='pb-[12rem] mt-[4rem] gap-6 flex flex-col justify-center items-center w-full h-full lg:flex lg:items-center lg:flex-col lg:justify-center lg:ml-[20%] lg:w-[80%] text-center'>
-      <Answers />
+      <Answers/>
     </main>
   );
 }
