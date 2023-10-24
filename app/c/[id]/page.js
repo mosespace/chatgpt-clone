f4b5d693ee494e3c6b60726601bef2afb04f65f2
mosespace <kisakyemoses50@gmail.com>
2023-10-24 09:37:04 +0300

    feat: initial commit

--- app/c/[id]/page.js
+++ app/c/[id]/page.js
@@ -1,13 +1,17 @@
+"use client";
 import React from "react";
 import getChat from "@/libs/getChat";
+import getSingleChat from "@/libs/getChat";
 import DetailedPage from "@/components/DetailedPage";
 
-export default async function Detailed() {
-  const singleChat = await getChat();
-  console.log(singleChat);
+export default async function Detailed({ params: { id } }) {
+  const singleChat = await getSingleChat(id);
+  // console.log(singleChat);
+  // console.log(singleChat);
   return (
     <div className='pb-[12rem] mt-[4rem] gap-6 flex flex-col justify-center items-center w-full h-full lg:flex lg:items-center lg:flex-col lg:justify-center lg:ml-[20%] lg:w-[80%] text-center'>
-      <DetailedPage data={singleChat} />
+      <DetailedPage singleChat={singleChat} />
+      {/* <h2>HELLO WORLD</h2> */}
     </div>
   );
 }
