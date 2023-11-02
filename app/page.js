74b1d7273db4963b26128f6b6eff99ee478cab1d
mosespace <kisakyemoses50@gmail.com>
2023-11-02 17:15:55 +0300

    feat: initial commit

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import HomeIntro from "@/components/HomeIntro";
 
 export default async function Home() {
   return (
-    <main className='mx-0 gap-6 flex flex-col items-center w-full h-full lg:flex lg:items-center lg:flex-col justify-start pt-[4rem] text-center bg-red-700'>
+    <main className='mx-0 gap-6 flex flex-col items-center w-full h-full lg:flex lg:items-center lg:flex-col justify-start pt-[4rem] text-center'>
       <HomeIntro />
     </main>
   );
