c97c3f94cb8c047159529aee9beb8fab633c8e22
mosespace <kisakyemoses50@gmail.com>
2023-11-02 18:31:31 +0300

    feat: initial commit

--- app/c/[id]/page.js
+++ app/c/[id]/page.js
@@ -5,10 +5,9 @@ export default async function Detailed({ params: { id } }) {
   const singleChat = await getSingleChat(id);
   // console.log(singleChat);
   return (
-    // <div className='pb-[12rem] mt-[4rem] gap-6 flex flex-col justify-center items-center w-full  h-full lg:flex lg:items-center lg:flex-col lg:justify-center lg:ml-[20%] lg:w-[82%] text-center'>
-    //   
-    //   {/* <DetailedPage singleChat={singleChat} /> */}
-    // </div>
-    <>testing</>
+    <div className='pb-[12rem] mt-[7.5rem] lg:mt-[4rem] gap-6 flex flex-col justify-center items-center w-full h-full lg:flex lg:items-center lg:flex-col lg:justify-center text-center'>
+      <DetailedPage singleChat={singleChat} />
+    </div>
   );
 }
+// lg:bg-purple-700
