bd83192f90f4d6f9726fa04ffb48244faf3776a8
mosespace <kisakyemoses50@gmail.com>
2023-11-02 09:25:35 +0300

    feat: initial commit

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import Btns from "./homeBtns/Btns";
 import Logo from "../public/Logo.svg";
-import Gpt4 from "./homeBtns/Gpt4";
-import Gpt5 from "./homeBtns/Gpt5";
 
 export default function Answers() {
   return (
     <>
-      <div className='bg-slate-300 flex justify-between items-center rounded-md px-[1rem] py-[.6rem] w-full font-bold  lg:w-[30%]'>
-        <Gpt4 />
-        <Gpt5 />
+      <div className='mx-[.3rem] lg:mx-0'>
+        <Btns />
       </div>
 
-      <div className='relative w-[150px] h-[150px]'>
+      <div className='relative w-[120px] h-[120px]'>
         <Image src={Logo} alt='AI ChatGPT-4' />
       </div>
     </>
