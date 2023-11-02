eaa41b38c0a93cd2334601663e27e4151c30db0a
mosespace <kisakyemoses50@gmail.com>
2023-11-02 09:26:02 +0300

    feat: initial commit

--- components/DetailedPage.jsx
+++ components/DetailedPage.jsx
@@ -1,18 +1,22 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import Logo from "../public/Logo.svg";
+import { useUser } from "@clerk/nextjs";
 import { GoPaste } from "react-icons/go";
 import { CiShare1 } from "react-icons/ci";
-import Profile from "../public/profile.jpg";
 import { SlLike, SlDislike } from "react-icons/sl";
 
 export default function Answers({ singleChat }) {
+  const { user } = useUser();
+  console.log(user);
+
   return (
     <div className='overflow-hidden'>
       {/*Response Section */}
 
-      <div className='bg-gray-200/60 w-full lg:absolute lg:z-50 lg:top-0 lg:w-[80%] border-2'>
-        <div className='flex justify-between py-[1rem] w-full px-[2rem]'>
+      <div className='bg-white lg:absolute overflow-hidden lg:z--50 lg:top-0 lg:left-0 lg:right-0 lg:border-2'>
+        <div className='flex justify-between py-[1rem] px-[2rem]'>
           <div className='hidden lg:block opacity-0 '>Hidden</div>
           <span>Default (GPT-3.5)</span>
           <button>
@@ -23,7 +27,7 @@ export default function Answers({ singleChat }) {
 
       {/* <div className='mt-[2rem] gap-6 flex flex-col justify-between items-center w-full'> */}
       {/*Question */}
-      {singleChat.conversation.map((single_conversation, id) => {
+      {singleChat?.conversation?.map((single_conversation, id) => {
         return (
           <div
             className='mt-[2rem] gap-6 flex flex-col justify-between items-center w-full'
@@ -31,7 +35,12 @@ export default function Answers({ singleChat }) {
           >
             <div className='flex gap-[1rem] lg:justify-normal justify-between py-[.6rem] w-full font-bold px-[2rem] lg:w-[60%]'>
               <div className='relative max-w-[50px] lg:max-w-[55px] max-h-[40px] lg:max-h-[55px]'>
-                <Image src={Profile} alt='user-name using AI ChatGPT-4' />
+                <Image
+                  src={user?.imageUrl}
+                  alt='user-name using AI ChatGPT-4'
+                  fill
+                  className='w-full h-full object-cover'
+                />
               </div>
               <div className='text-left' style={{ flex: 1 }}>
                 <h2 className='text-left'>{single_conversation.prompt}</h2>
