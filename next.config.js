b52184464b408e497fa32f65b749abef7d429f4f
mosespace <kisakyemoses50@gmail.com>
2023-11-02 09:21:02 +0300

    feat: initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
+module.exports = {
+  images: {
+    domains: ["img.clerk.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
