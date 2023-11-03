e0618e04c5ada8a90beb0a7c88ce7e151157ccca
mosespace <kisakyemoses50@gmail.com>
2023-11-03 13:01:59 +0300

    feat: initial commit

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -2,8 +2,10 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className='absolute backdrop-blur-sm bg-white w-full h-screen z-50 flex items-center justify-center'>
-      <SignIn />
+    <div className='absolute bg-bg bg-no-repeat bg-cover bg-green w-screen h-screen z-[230] left-0 right-0 flex items-center justify-center overflow-hidden'>
+      <div className='backdrop-blur-sm w-screen flex items-center justify-center h-screen'>
+        <SignIn />
+      </div>
     </div>
   );
 }
