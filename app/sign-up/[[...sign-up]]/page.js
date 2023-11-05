c1619b69447833f2e1f150bf41244e158f44618e
mosespace <kisakyemoses50@gmail.com>
2023-11-05 11:34:45 +0300

    feat: initial commit

--- app/sign-up/[[...sign-up]]/page.js
+++ app/sign-up/[[...sign-up]]/page.js
@@ -1,5 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className='absolute bg-bg bg-no-repeat bg-cover bg-green w-screen h-screen z-[230] left-0 right-0 flex items-center justify-center overflow-hidden'>
+      <div className='backdrop-blur-sm w-screen flex items-center justify-center h-screen'>
+        <SignUp />
+      </div>
+    </div>
+  );
 }
